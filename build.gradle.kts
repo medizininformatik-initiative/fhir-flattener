e3b6425d1ecb1f07855f8e95e089f51ad343bd7d
Johannes Oehm <johannes.oehm@uni-muenster.de>
2025-10-29 14:29:49 +0100

    updating dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.kotlin.dsl.withType
 
 plugins {
-    kotlin("jvm") version "2.2.20"
-    id("com.gradleup.shadow") version "8.3.8"
-    kotlin("plugin.serialization") version "2.2.20"
+    kotlin("jvm") version "2.2.21"
+    id("com.gradleup.shadow") version "9.2.2"
+    kotlin("plugin.serialization") version "2.2.21"
 }
 
 group = "ms.uni.imi.medic"
@@ -14,21 +14,25 @@ repositories {
     mavenCentral()
 }
 
+val ktorVersion = "3.3.1"
+val pathlingVersion = "8.1.0"
+
 dependencies {
-    implementation("io.ktor:ktor-server-core:3.3.0")
-    implementation("io.ktor:ktor-server-host-common:3.3.0")
-    implementation("io.ktor:ktor-server-status-pages:3.3.0")
-    implementation("io.ktor:ktor-server-core:3.3.0")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-host-common:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+
     testImplementation(kotlin("test"))
 
-    implementation("au.csiro.pathling:library-runtime:8.0.2")
-    implementation("au.csiro.pathling:fhirpath:8.0.2")
-    implementation("au.csiro.pathling:terminology:8.0.2")
+    implementation("au.csiro.pathling:library-runtime:$pathlingVersion")
+    implementation("au.csiro.pathling:fhirpath:$pathlingVersion")
+    implementation("au.csiro.pathling:terminology:$pathlingVersion")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 
-    implementation("io.ktor:ktor-server:3.3.0")
-    implementation("io.ktor:ktor-server-netty:3.3.0")
 }
 
 tasks.test {
